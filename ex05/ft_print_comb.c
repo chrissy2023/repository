43dd5d0fd1ae87131dc686bc4486a731c0116175
chrissy2023 <153122174+chrissy2023@users.noreply.github.com>
2023-12-06 19:27:29 +0100

    Add files via upload

---
+++ ex05/ft_print_comb.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cparovel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 17:44:06 by cparovel          #+#    #+#             */
+/*   Updated: 2023/12/06 12:12:15 by cparovel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putchar1(char n1, char n2, char n3)
+{
+	ft_putchar(n1);
+	ft_putchar(n2);
+	ft_putchar(n3);
+	if (n1 == '7' && n2 == '8' && n3 == '9')
+	{
+		write(1, "", 1);
+	}
+	else
+	{
+		write(1, ", ", 2);
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char	n1;
+	char	n2;
+	char	n3;
+
+	n1 = '0';
+	while (n1 <= '7')
+	{
+		n2 = n1 + 1;
+		while (n2 <= '8')
+		{
+			n3 = n2 + 1;
+			while (n3 <= '9')
+			{
+				ft_putchar1(n1, n2, n3);
+				n3++;
+			}
+			n2++;
+		}
+		n1++;
+	}
+	ft_putchar('\n');
+}
---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cparovel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 21:01:09 by cparovel          #+#    #+#             */
+/*   Updated: 2023/12/06 12:09:37 by cparovel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			ft_putchar(i / 10 + '0');
+			ft_putchar(i % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(j / 10 + '0');
+			ft_putchar(j % 10 + '0');
+			if (i != 98 || i != 99)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	ft_putchar('\n');
+}
